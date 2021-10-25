4cb7142c8e728669c16e0f63be0b447bf2c94cd5
KevaughnSimmonds <92341333+KevaughnSimmonds@users.noreply.github.com>
2021-10-24 19:33:32 -0500

    Delete TransactionLogs.java

--- GrizzlyEntertainment/src/logging/TransactionLogs.java
+++
@@ -1,8 +0,0 @@
-package logging;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class TransactionLogs {
-	private static final Logger logger = LogManager.getLogger(TransactionLogs.class);
-}
